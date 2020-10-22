dc58aacb14dff708afe1f3b654aa12c04a2e44d3
Monesh321 <54687625+Monesh321@users.noreply.github.com>
2020-10-22 14:45:45 +0530

    Add files via upload

--- test_07_contact_us.py
+++ test_07_contact_us.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from Pages.contact_us import contact_us
 
-baseurl = "https://www.sunlifeglobalinvestments.com/Slgi/Contact+us?vgnLocale=en_CA"
+baseurl = "https://www.slcmanagement.com/inv/Contact+us?vgnLocale=en_CA"
 
 
 # client sign in
@@ -12,12 +12,14 @@ baseurl = "https://www.sunlifeglobalinvestments.com/Slgi/Contact+us?vgnLocale=en
 def driver(request):
     chrome_options = Options()
     # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--start-maximized")
     # chrome_options.add_argument(f"--window-size={1920},{3926}")
     # chrome_options.add_argument("--hide-scrollbars")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(executable_path="/home/monesh/PycharmProjects/SLCM_sunlife/chromedriver",
+                              options=chrome_options)
     driver.implicitly_wait(8)
     driver.get(baseurl)
 
@@ -37,31 +39,27 @@ def contact(driver):
 
 @pytest.mark.usefixtures("driver", "contact")
 class Tests():
-    def test_37_verify_url7_title(self, contact):
+    def test_31_verify_url7_title(self, contact):
         time.sleep(3)
         contact.verify_title()
 
-    def test_38_verify_header(self, contact):
-        time.sleep(1)
-        contact.verify_header_present()
+    def test_32_validate_Footer_links(self, driver, contact):
+        contact.verify_footer_quicklinks("footer_links", breadcrumb_status=True)
 
-    def test_39_verify_breadcrumb(self, contact):
-        contact.verify_breadcrumb_active()
+    def test_33_verify_disclosure_text(self, driver, contact):
+        contact.verify_disclosure()
 
-    def test_40_verify_phone_displayed(self, contact):
+    def test_34_verify_breadcrumb(self, contact):
+        contact.verify_breadcrumb()
+
+    def test_35_verify_page_header_text(self, contact):
+        contact.verify_page_header_text()
+
+    def test_36_verify_phone_displayed(self, contact):
         contact.verify_phone_displayed()
 
-    def test_41_verify_fax_displayed(self, contact):
-        contact.verify_fax_displayed()
+    def test_37_validate_contact_us_form(self, contact):
+        contact.fill_and_submit_contact_us_form()
 
-    def test_42_verify_email_displayed(self, contact):
-        contact.verify_email_displayed()
-
-    def test_43_verify_Question_1(self, contact):
-        contact.verify_Question_1()
-
-    def test_44_verify_Question_2(self, contact):
-        contact.verify_Question_2()
-
-    def test_45_verify_Question_3(self, contact):
-        contact.verify_Question_3()
+if __name__ == '__main__':
+    pytest.main()
